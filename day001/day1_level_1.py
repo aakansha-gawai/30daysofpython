f0ca1026666bc1d598bade4966d6d433a07979a8
aakansha-gawai <aakansha.gawai@zohomail.in>
2023-11-24 20:35:56 +0530

    added solution for day1 level1

--- day001/day1_level_1.py
+++ day001/day1_level_1.py
@@ -1,2 +1,25 @@
 # Level File
-print('Hello Aakansha')
+print('Hello Aakansha') 
+
+#introduction
+#day1 -30 days python challenge 
+print(2+3) #addition test 
+print(3-1) #substraction test 
+print(2*3) #multiplication test  
+print(3/2) #division test
+print(3**2) #exponential test
+print(3//2)#floor dividional operator 
+print("hello poonam ,aakansha here")
+print(9**9)
+print(9//4)
+
+#checking data types 
+
+print(type(10))       #int
+print(type(3.14))     # Float
+print(type(1+3j))     #complex 
+print(type('name')) #string 
+print(([1,2,3,4,5,6,7,8,9])) #list 
+print(type({'aaksnha':'poonam'})) #dictionary 
+print(([1.2,2.3,9.4,7.14])) #set 
+print(((1.2,7.6,0.9)))      #tuple 
