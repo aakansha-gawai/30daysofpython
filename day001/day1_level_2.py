873ec3370c02625fec65bf88778c32b8ce321d4c
aakansha-gawai <aakansha.gawai@zohomail.in>
2023-11-25 20:49:18 +0530

    here is new practice session for python day1

---
+++ day001/day1_level_2.py
@@ -0,0 +1,23 @@
+#30 days of python 
+#intro and exercise
+
+print('Hello aakansha')  #string 
+print(3+2)  #addition
+print(7-6)  #substraction
+print(7*3)  #multiplicstion
+print(9/4)  #division
+print(4**5) #exponenntial 
+print(9%7)  #modulus 
+print(7//3) #floor dividion operator 
+
+
+#checking data types 
+
+print(type(10))  #integer
+print(type(8.9))  #float 
+print(type(1+9j)) #complex 
+print(type('aakansha'))  #string
+print(type(1,2,3,4,5,6,7,8,9)) #list
+print(type ('aakansha','poonam'))   #dictionary
+print(type({1,1.2,7,6.5}))         #set 
+print(type(((1,2.3,4,5.6))))       #tuple
